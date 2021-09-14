669030d53c7b35b294e80dbb82db47c8ab858c4c
Kathy Kuo <dudoo.kathy@gmail.com>
2021-09-14 14:06:05 +0800

    新增首頁

---
+++ components/Footer.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="footer">
+    <div>
+      <div>雲林浤賀蔬果</div>
+      <div></div>
+      <div>
+        <p>----- 蔬果．花卉進口 -----</p>
+        <p>----- 蔬果批發．零售 -----</p>
+        <p>----- 農產品買賣 -----</p>
+      </div>
+    </div>
+    <div class="column">
+      <div class="content_box">
+        <div>
+          <p>地址</p>
+        </div>
+        <div>
+          <p>雲林縣虎尾鎮文科路1342號</p>
+        </div>
+      </div>
+      <div class="content_box">
+        <div>
+          <p>統一編號</p>
+        </div>
+        <div>
+          <p>42803868</p>
+        </div>
+      </div>
+      <div class="content_box">
+        <div>
+          <p>聯絡電話</p>
+        </div>
+        <div>
+          <a href="tel:+055880633">055880633</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
