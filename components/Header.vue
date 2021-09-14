062ddc44e65743f25aa64d852ac772a1878d9bbb
Kathy Kuo <dudoo.kathy@gmail.com>
2021-09-14 14:06:05 +0800

    新增首頁

---
+++ components/Header.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="header">
+        <div class="header_title"><img src="~static/img/logo.jpeg" alt="" />浤賀有限公司</div>
+        <div class="menu_list">
+            <ul>
+                <li>
+                    <a href="#title1">首頁</a>
+                </li>
+                <li>
+                    <a href="#title2">關於我們</a>
+                </li>
+                <li>
+                    <a href="#title3">產品</a>
+                </li>
+                <li>
+                    <a href="#title4">合作</a>
+                </li>
+                <li>
+                    <a href="#title5">聯絡我們</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,134 @@
+<template>
+  <div class="wrapper">
+    <Header></Header>
+    <div class="wrap_main">
+      <div class="top_view" id="title1">
+        <div class="info">
+          <div class="top_title"><h1>雲林最大蔬果經銷商</h1></div>
+          <div class="top_message"><h2>蔬果批發｜蔬果進口</h2></div>
+          <div class="tabkeALook_btn">FB粉絲專頁</div>
+        </div>
+      </div>
+      <div class="first_view">
+        <div class="cardList">
+          <div class="card">
+            <div>
+              <img src="~/static/img/icon/icon1.png" alt="" />
+              <h3>國際貿易</h3>
+              <h4>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quasi,
+                necessitatibus!
+              </h4>
+            </div>
+          </div>
+          <div class="card">
+            <div>
+              <img src="~/static/img/icon/icon2.png" alt="" />
+              <h3>信用交易</h3>
+              <h4>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quasi,
+                necessitatibus!
+              </h4>
+            </div>
+          </div>
+          <div class="card">
+            <div>
+              <img src="~/static/img/icon/icon3.png" alt="" />
+              <h3>高品質</h3>
+              <h4>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quasi,
+                necessitatibus!
+              </h4>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="second_view" id="title2">
+        <div>
+          <div>
+            <img src="~/static/img/main.jpeg" alt="" />
+          </div>
+          <div>
+            <div class="sencond_title">關於我們</div>
+            <div class="sencond_message">
+              <p>
+                成立於2018年，雲林縣最大蔬菜貿易公司，進口批售大白菜、白蘿蔔、高麗菜、青花菜、美生菜、南瓜等蔬果。主要販售的通路有果菜市場、餐廳食材等。
+              </p>
+              <p>地址：雲林縣虎尾鎮文科路1342號</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="img_view">
+        <div class="card" v-for="(item, index) in products" :key="index">
+          <div class="card_message">
+            <h3>{{ item.name }}</h3>
+            <h4>{{ item.info }}</h4>
+          </div>
+        </div>
+      </div>
+    </div>
+    <Footer id="title7"></Footer>
+    <!-- <div id="fb-root"></div> -->
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      products: [
+        {
+          name: "大白菜",
+          title: "冬季的蔬菜王",
+          info: "結球白菜、大白菜、捲心白菜、黃芽白菜、山東大白菜、天津白菜、包心白菜、洋白菜",
+          href: "img/v1.png",
+          img: "img/v1_1.jpeg",
+        },
+        {
+          name: "白蘿蔔",
+          title: "十一月十二月",
+          info: "新竹：新竹市、五峰，台中：清水，雲林，南投：埔里，台南，彰化縣，嘉義縣",
+          href: "img/v2.png",
+          img: "img/v2_1.jpeg",
+        },
+        {
+          name: "高麗菜",
+          title: "產地來源",
+          info: "彰化、雲林",
+          href: "img/v3.png",
+          img: "img/v3_1.jpeg",
+        },
+        {
+          name: "青花菜",
+          title: "十一至四月",
+          info: "嘉義、彰化、雲林",
+          href: "img/v4.png",
+          img: "img/v4_1.jpeg",
+        },
+        {
+          name: "美生菜",
+          title: "產地來源",
+          info: "品種介紹",
+          href: "img/v5.png",
+          img: "img/v5_1.jpeg",
+        },
+        {
+          name: "南瓜",
+          title: "產地來源",
+          info: "品種介紹",
+          href: "img/v6.png",
+          img: "img/v6_1.jpeg",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+@import "~assets/css/global.min.css";
+@import "~assets/css/components/header.min.css";
+@import "~assets/css/components/footer.min.css";
+@import "~assets/css/HongHe/style.min.css";
+</style>
