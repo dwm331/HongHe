8988416fa13dcc00ad91b3dc227370ccb4b1885a
dwm331 <t9618006@ntut.org.tw>
2021-11-17 14:04:08 +0800

    3719754

---
+++ _nuxt/2b01da6.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{114:function(e,o,n){"use strict";var c=n(1),t=n(161),_=n.n(t);c.a.use(_.a,{page_id:"196766557402879",theme_color:"#6699cc",locale:"zh_TW",logged_out_greeting:"嗨~有問題可以私訊我喔！",logged_in_greeting:"您好，很高興為您服務"})},164:function(e,o,n){n(165),e.exports=n(166)}},[[164,8,1,9]]]);
---
+++ _nuxt/static/1637129016/index2/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002FHongHe\u002F_nuxt\u002Fstatic\u002F1637129016",layout:"default",error:a,serverRendered:true,routePath:"\u002Findex2",config:{_app:{basePath:"\u002FHongHe\u002F",assetsPath:"\u002FHongHe\u002F_nuxt\u002F",cdnURL:a}}}}(null));
